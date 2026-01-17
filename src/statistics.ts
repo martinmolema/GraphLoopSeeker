85219f12ac44bef561c086047776cd973fe829d6
Martin Molema <martin@molema.org>
2026-01-17 12:30:08 +0100

    more examples

---
+++ src/statistics.ts
@@ -0,0 +1,8 @@
+export type Statistics = {
+    nrOfNodes: number;
+    nrOfEdges: number;
+    longestPath: number;
+    maxLevelOfRecursion: number;
+    nrOfSearchIterations: number;
+    nrOfLoopsFound: number;
+}
